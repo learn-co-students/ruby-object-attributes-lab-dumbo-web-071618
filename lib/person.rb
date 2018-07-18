f85056c9ed738e7a63264b1951b975564dca338e
Kyle-Neale <kyle.a.neale@gmail.com>
2018-07-18 13:39:43 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+
+
+class Person
+  def name
+    @name
+  end
+
+  def name=(set_name)
+    @name = set_name
+  end
+
+    def job
+      @job
+    end
+
+    def job=(set_job)
+      @job = set_job
+    end
+end
