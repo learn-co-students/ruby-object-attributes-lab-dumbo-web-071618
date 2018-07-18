0057986229faa880bcf4fe0b8a75f85fc2330cf5
Bryan Teng <bteng1226@gmail.com>
2018-07-18 17:34:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name
+    @name
+  end
+  
+  def name=(string)
+    @name = string
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
